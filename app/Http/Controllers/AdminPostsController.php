1e52dfc1b6803bed3992e2b9ef87cc531238e750
rashedraju <rashedraju.cs@gmail.com>
2021-12-23 19:07:58 +0600

    refactor code

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,74 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AdminPostRequest;
 use App\Models\Post;
 use App\Models\User;
-use App\Http\Requests\AdminPostRequest;
 
-class AdminPostsController extends Controller
-{
+class AdminPostsController extends Controller {
 
-    public function __construct()
-    {
-        $this->authorizeResource(Post::class);
+    public function __construct() {
+        $this->authorizeResource( Post::class );
     }
 
-    public function index()
-    {
-        return view('admin.posts.index', ['posts' => Post::all()]);
+    public function index() {
+        return view( 'admin.posts.index', ['posts' => Post::all()] );
     }
 
-    public function create()
-    {
-        return view('admin.posts.create');
+    public function create() {
+        return view( 'admin.posts.create' );
     }
 
-    public function store(AdminPostRequest $request)
-    {
+    public function store( AdminPostRequest $request ) {
         $attributes = $request->validated();
 
-        if($request->hasFile('thumbnail')){
-            $path = $request->file('thumbnail')->store("thumbnails");
+        if ( $request->hasFile( 'thumbnail' ) ) {
+            $path = $request->file( 'thumbnail' )->store( "thumbnails" );
 
-            $attributes = array_merge($attributes, [
-                'user_id' => auth()->user()->id,
+            $attributes = array_merge( $attributes, [
+                'user_id'   => auth()->user()->id,
                 'thumbnail' => $path
-            ]);
+            ] );
         }
 
-        Post::create($attributes);
+        Post::create( $attributes );
 
-        return redirect()->route('admin.posts.index')->with('success', 'Post is successfully created');
+        return redirect()->route( 'admin.posts.index' )->with( 'success', 'Post is successfully created' );
     }
 
-    public function edit(Post $post)
-    {
+    public function edit( Post $post ) {
 
-        return view('admin/posts/edit', ['post' => $post]);
+        return view( 'admin/posts/edit', ['post' => $post] );
     }
 
-    public function update(AdminPostRequest $request, Post $post)
-    {
+    public function update( AdminPostRequest $request, Post $post ) {
 
         $attributes = $request->validated();
 
-        if($request->hasFile('thumbnail')){
-            $path = $request->file('thumbnail')->store("thumbnails");
+        if ( $request->hasFile( 'thumbnail' ) ) {
+            $path = $request->file( 'thumbnail' )->store( "thumbnails" );
 
-            $attributes = array_merge($attributes, [
+            $attributes = array_merge( $attributes, [
                 'thumbnail' => $path
-            ]);
+            ] );
         }
 
-        $post->update($attributes);
+        $post->update( $attributes );
 
-        return redirect()->route('admin.posts.index')->with('success', 'Post updated');
+        return redirect()->route( 'admin.posts.index' )->with( 'success', 'Post updated' );
     }
 
-    public function destroy(Post $post)
-    {
+    public function destroy( Post $post ) {
         $post->delete();
 
-        return redirect()->back()->with('success', 'Post Deleted');
+        return redirect()->back()->with( 'success', 'Post Deleted' );
     }
 }
