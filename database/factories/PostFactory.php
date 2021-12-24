f9fc611e86348c0d3b1cc8e9b9e30ac29b86e93a
rashedraju <rashedraju.cs@gmail.com>
2021-12-24 20:56:07 +0600

    modified post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,27 +3,25 @@
 namespace Database\Factories;
 
 use App\Models\Category;
-use App\Models\Status;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class PostFactory extends Factory
-{
+class PostFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array
      */
-    public function definition()
-    {
+    public function definition() {
         return [
-            "user_id" => User::factory(),
+            "user_id"     => User::factory(),
             "category_id" => Category::factory(),
-            "title" => $this->faker->sentence(),
-            'thumbnail' => 'thumbnails\ChVR4ZCDhRMB9yVljrrw8LEWT4dnA0zsI3pLotLl.png',
-            "slug" => $this->faker->slug(),
-            "excerpt" => $this->faker->sentence(),
-            "body" => $this->faker->paragraph(),
+            "title"       => $this->faker->sentence(),
+            'thumbnail'   => 'thumbnails\illustration-1.png',
+            "slug"        => $this->faker->slug(),
+            "excerpt"     => $this->faker->sentence(),
+            "status_id"   => 2,
+            "body"        => $this->faker->paragraph()
         ];
     }
 }
